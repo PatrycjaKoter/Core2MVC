317bb391a6789d6d0fd0daf40829230861d87485
PatrycjaKoter <patrycjakoter@gmail.com>
2019-07-30 19:23:15 +0200

    Add basic image, bootstrap and css

--- PieShop/PieShop/Controllers/HomeController.cs
+++ PieShop/PieShop/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,26 @@ namespace PieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var pies = _pieRepository.GetAllPies().OrderBy(pie => pie.Name);
+
+            var homeViewModel = new HomeViewModel()
+                {
+                Title = "Welcome to Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
         }
     }
 }
--- PieShop/PieShop/Startup.cs
+++ PieShop/PieShop/Startup.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+using PieShop.Models;
 
 namespace PieShop
 {
@@ -12,6 +12,7 @@ namespace PieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddMvc();
         }
 
